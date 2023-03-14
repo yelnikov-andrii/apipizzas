e1f861c50b71120aa3a848b04b2ad2ffe1d27fdf
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-03-15 00:06:01 +0200

    added auth rewrite products with sequelize

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { orderController } from '../controllers/orderController.js';
-import { authMiddleware } from '../middleWares/authMiddleware.js';
+// import { authMiddleware } from '../middleWares/authMiddleware.js';
 import { catchError } from '../utils/catchError.js';
 
 export const ordersRouter = express.Router();
