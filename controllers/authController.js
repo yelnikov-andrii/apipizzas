0ec388fed87f3fbf30328b5cbd8453ee7d2aa0a6
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-03-15 16:56:00 +0200

    cookies dont getting on client

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,6 +72,8 @@ async function sendAuthentication(res, user) {
   const refreshToken = jwtService.generateRefreshToken(userData);
   await tokenService.save(user.id, refreshToken);
   res.cookie('refreshToken', refreshToken, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
     sameSite: 'none',
     secure: true
   })
--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  // credentials: true,
-  // origin: 'http://localhost:3000'
-  // origin:
-  //   process.env.NODE_ENV === "production"
-  //     ? process.env.CLIENT_URL
-  //     : "http://localhost:3000",
+  credentials: true,
+  // origin: 'https://yelnikov-andrii.github.io/pizza2'
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.CLIENT_URL
+      : "http://localhost:3000",
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true} ));
