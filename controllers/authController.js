48b2ed2453114a3bd95f5498c4e19e60f48db9e5
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-03-15 19:20:59 +0200

    cookies dont getting on client

--- controllers/authController.js
+++ controllers/authController.js
@@ -84,6 +84,7 @@ async function sendAuthentication(res, user) {
 async function refresh(req, res) {
   const { refreshToken } = req.cookies;
   const userData = jwtService.validateRefreshToken(refreshToken);
+  res.clearCookie('refreshToken');
   console.log(userData, refreshToken)
   if (!userData) {
     throw ApiError.Unauthorized();
