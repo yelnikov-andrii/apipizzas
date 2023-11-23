448e281e3dec88032eae1e2a435b60fd83bca326
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-11-23 14:45:17 +0200

    remove http only in cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,7 +79,7 @@ async function sendAuthentication(res, user) {
 
   res.cookie('refreshToken', refreshToken, {
     maxAge: 1000 * 15,
-    // httpOnly: true,
+    httpOnly: true,
     // sameSite: 'none',
     // secure: true
   });
