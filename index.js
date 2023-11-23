6a833804e730f7e2ea486f48b2174d91f3901fa2
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-11-23 22:36:18 +0200

    try to fix credentials

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
   credentials: true,
   origin:
     process.env.NODE_ENV === "production"
-      ? ['https://yelnikov-andrii.github.io', 'https://pizza3.netlify.app']
+      ? ['https://yelnikov-andrii.github.io', 'https://pizza3.netlify.app', 'http://localhost:3000']
       : "http://localhost:3000",
 }));
 
