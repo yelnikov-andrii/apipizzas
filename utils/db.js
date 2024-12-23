87a404d186ee73debfb75c21b5689dfd7669d1de
Andrii <whiteyelnikov@gmail.com>
2024-12-23 12:52:36 +0200

    changed db

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-// export const sequelize = new Sequelize('mydatabase_27kd', 'mydatabase_27kd_user', 'RjN6wPs0H75Oz7DuOBU7aqxcoslMwcO8', {
-//   // host: 'dpg-coolt8v79t8c73f66d40-a.oregon-postgres.render.com',
-//   host: 'dpg-coolt8v79t8c73f66d40-a',
+// export const sequelize = new Sequelize('db_ct4e', 'db_ct4e_user', 'owytH5C8f6TjqKt2DB3rEmv78zGcZPO4', {
+//   // host: 'dpg-cr87bujqf0us73braolg-a.oregon-postgres.render.com',
+//   host: 'dpg-cr87bujqf0us73braolg-a',
 //   dialectOptions: {
 //     ssl: {
 //       require: true,
@@ -13,16 +13,9 @@ import { Sequelize } from 'sequelize';
 //   logging: false,
 // });
 
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (error) {
-//   console.error('Unable to connect to the database:', error);
-// }
-
-export const sequelize = new Sequelize('db_ct4e', 'db_ct4e_user', 'owytH5C8f6TjqKt2DB3rEmv78zGcZPO4', {
-  // host: 'dpg-cr87bujqf0us73braolg-a.oregon-postgres.render.com',
-  host: 'dpg-cr87bujqf0us73braolg-a',
+export const sequelize = new Sequelize('december_db', 'december_db_user', 'CSXkeLATZYYS1lZtcVyOLnV0TulCqbbj', {
+  // host: 'dpg-ctkjugi3esus73e8nmk0-a.oregon-postgres.render.com',
+  host: 'dpg-ctkjugi3esus73e8nmk0-a',
   dialectOptions: {
     ssl: {
       require: true,
